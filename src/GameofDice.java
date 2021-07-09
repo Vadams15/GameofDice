f5418757f07f57bd65ea72579ab32bb16e6a323c
VAdams15 <adamsval5@gmail.com>
2021-07-09 12:21:53 -0500

    working on solutions

---
+++ src/GameofDice.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+public class GameofDice {
+    public static void main(String... args); {
+    }
+    Scanner Keyboard = new Scanner(System.in);
+        System.out.println("Welcome to Roll the Dice!");
+
+        do {
+            Random r = new Random ();
+            int x = 1 + r.nextInt (1);
+            int i = 1 + r.nextInt (2);
+
+            if ( x + i <= 100);
+            System.out.println("Roll Again?");
+            System.out.println("Continue?(y/n: ");
+            if ("y"); ()
+            System.out.println("Roll Again?");
+            if ("yes");
+            System.out.println("Roll");
+            if "no";
+            System.out.println(x + i);
+            else if (x + i = 100);
+            System.out.println("Congratulations!");
+}
